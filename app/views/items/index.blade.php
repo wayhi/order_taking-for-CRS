feb95eeffe75d882cc777aeefed5111ec53efd8c
wayhi <JW@shiwenbodeMacBook-Pro.local>
2015-03-10 19:44:03 +0800

    March 10th update1

--- app/views/items/index.blade.php
+++ app/views/items/index.blade.php
@@ -1,5 +1,6 @@
 @extends('default')
 @section('main')
+
 <script>
 $(document.body).on('hidden.bs.modal', function () {
     $('#myModal').removeData('bs.modal');
@@ -21,12 +22,14 @@ $(document).on('hidden.bs.modal', function (e) {
     </div>
   </div>
 </div>
+
 <div class="col-md-12">
 <div class="pull-left"><h3 style="margin-top:0;">全部产品</h3></div>
 
 <div class="pull-right li_menu">
 <a href="＃" class="li_menu_a">全部产品</a>　<a href="＃">限购产品</a>　<a href="＃">热销产品</a>
 </div>
+
 </div>
 
 @for($i=0;$i < count($items);$i=$i+3)
@@ -46,6 +49,29 @@ $(document).on('hidden.bs.modal', function (e) {
 </h4><br>
 <div style="clear:both"></div>
 <span class="label label-success">¥ {{$items[$i+$k]->price_actual}}</span> 
+
+
+</div>
+
+<div class="col-md-12 col-lg-12">
+@for($i=0;$i < count($items);$i=$i+3)
+<div class="row">
+@for($k=0;($k<3)&&($k+$i < count($items));$k++)
+<div class="col-md-4 col-lg-4">
+
+<div class="box">
+<div style="display: block; margin-left: 90px; margin-right: auto;">
+	<image src="{{$items[$i+$k]->image->url('thumbnail')}}" >
+	<br>
+</div>
+
+<div class=" lb_r">
+<h4>
+<div class="show1">{{$items[$i+$k]->category->name}}</div> <a href="#" target="_blank">{{$items[$i+$k]->item_name}}</a>
+</h4><br>
+<div style="clear:both"></div>
+<span class="label label-success">¥ {{$items[$i+$k]->price_actual}}</span> 
+ 
 
 @if($items[$i+$k]->activated==1)
 <div class='pull-right'>
@@ -68,9 +94,13 @@ $(document).on('hidden.bs.modal', function (e) {
 
 @endfor
 
-
 </div><!--end of row-->
 
 @endfor
+
+
+
+@endfor
+</div>
 <div class='pagination inline'>{{$items->links();}}</div>
 @stop
