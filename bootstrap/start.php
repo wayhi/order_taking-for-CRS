7fff92a1264a5db791932cf0aca0aa521280b57a
wayhi <JW@shiwenbodeMacBook-Pro.local>
2015-07-17 23:44:02 +0800

    0717home

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,6 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('shiwenbodeMacBook-Pro.local','JWs-Mac-mini-2.local'),
+	'stage' => array('stage_server'),
 
 ));
 
